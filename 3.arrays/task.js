a33696434b466d0b326e0d7e7056e92bf9fea239
IlyaHF <xal19.90@mail.ru>
2023-06-23 21:11:05 +0300

    дз 3: Массивы

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,14 @@
 function compareArrays(arr1, arr2) {
-
+return arr1.length == arr2.length && arr1.every((n, i) => arr2[i] == n);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  let filteredUsers = users.filter(user => user.gender === gender);
+  let usersAge = filteredUsers.map(user => user.age);
+
+  if (!users.length) {
+    return 0;
+  } else if (!filteredUsers.length) {
+    return 0;
+  } else return usersAge.reduce((summAge, ageValue) => summAge + ageValue, 0) / usersAge.length;
 }
